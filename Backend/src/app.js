58e2c31f72f0992895a1b8987a1f44f5a34c7485
mukulkumawat2412 <154887138+mukulkumawat2412@users.noreply.github.com>
2025-12-04 20:58:01 +0530

    update c

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,11 +13,9 @@ app.use("/uploads", express.static("public/temp"));
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+ 
 }));
 
-// ✅ Preflight fix (VERY IMPORTANT FOR VERCEL)
 
 
 
