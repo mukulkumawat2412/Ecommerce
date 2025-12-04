9b0dc080cebd1274df86f579d0c3fb44b79b7e45
mukulkumawat2412 <154887138+mukulkumawat2412@users.noreply.github.com>
2025-12-04 14:45:14 +0530

    updated

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { RefreshAccessToken, Logout } from "../redux/slices/authSlice.jsx";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
   withCredentials: true,
 });
 
