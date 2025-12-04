9f7f4f06f9c9afdcd2a8b191997cec8b6dd4f629
mukulkumawat2412 <154887138+mukulkumawat2412@users.noreply.github.com>
2025-12-04 20:55:07 +0530

    up code

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,31 +10,14 @@ const app = express();
 app.use(express.json());
 app.use("/uploads", express.static("public/temp"));
 
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
-
-const allowedOrigins = [
-  "https://ecommerce-rosy-three.vercel.app",
-  "http://localhost:5173"
-];
-
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
-
+// ✅ Preflight fix (VERY IMPORTANT FOR VERCEL)
 
 
 
