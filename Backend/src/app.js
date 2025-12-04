cadd10dc6faba74b84d007a144d0e7a698440ff7
mukulkumawat2412 <154887138+mukulkumawat2412@users.noreply.github.com>
2025-12-04 19:52:53 +0530

    updated code

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,8 +13,7 @@ app.use("/uploads", express.static("public/temp"));
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+ 
 }));
 
 
