ef5904950ce0916f924d3b73fa6addd9a9a0a7fd
mukulkumawat2412 <154887138+mukulkumawat2412@users.noreply.github.com>
2026-04-23 12:43:56 +0530

    Remove SysTemLogout route from user.route.js
    Removed SysTemLogout route from user routes.

--- Backend/src/Routes/user.route.js
+++ Backend/src/Routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { Login ,Logout,RefreshAccessToken,Register,Profile, UpdateProfile, ProfileChangePassword,SysTemLogout} from "../controllers/user.controller.js";
+import { Login ,Logout,RefreshAccessToken,Register,Profile, UpdateProfile, ProfileChangePassword} from "../controllers/user.controller.js";
 
 import verifyToken from "../middleware/auth.middleware.js";
 import authorizeRoles from "../middleware/checkRole.js";
@@ -14,7 +14,6 @@ const router = Router()
 router.route("/register").post(Register)
 
 router.route("/login").post(Login)
-router.post('/Slogout', SysTemLogout);
 router.route("/profile").get(verifyToken,authorizeRoles(["admin","user"]),Profile)
 router.route("/profile-update").put(verifyToken,authorizeRoles(["admin","user"]),UpdateProfile)
 router.route("/profile-change-password").put(verifyToken,authorizeRoles(["admin","user"]),ProfileChangePassword)
