d5c05723c948abe7d3f14cba8d6ea3cc46bd926e
mukulkumawat2412 <154887138+mukulkumawat2412@users.noreply.github.com>
2026-04-23 12:25:17 +0530

    Remove systemA and systemB login routes
    Removed systemA and systemB login routes from user.route.js.

--- Backend/src/Routes/user.route.js
+++ Backend/src/Routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { Login ,Logout,RefreshAccessToken,Register,Profile, UpdateProfile, ProfileChangePassword,systemBLogin,systemALogin,SysTemLogout} from "../controllers/user.controller.js";
+import { Login ,Logout,RefreshAccessToken,Register,Profile, UpdateProfile, ProfileChangePassword,SysTemLogout} from "../controllers/user.controller.js";
 
 import verifyToken from "../middleware/auth.middleware.js";
 import authorizeRoles from "../middleware/checkRole.js";
@@ -14,8 +14,6 @@ const router = Router()
 router.route("/register").post(Register)
 
 router.route("/login").post(Login)
-router.post('/systemA/login', systemALogin);
-router.post('/systemB/login', systemBLogin);
 router.post('/Slogout', SysTemLogout);
 router.route("/profile").get(verifyToken,authorizeRoles(["admin","user"]),Profile)
 router.route("/profile-update").put(verifyToken,authorizeRoles(["admin","user"]),UpdateProfile)
