a2b3b97f5202f7f08b8121d78eb361b03ae24b7b
mukulkumawat2412 <154887138+mukulkumawat2412@users.noreply.github.com>
2026-04-23 11:51:56 +0530

    Add system login routes to user.route.js

--- Backend/src/Routes/user.route.js
+++ Backend/src/Routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { Login ,Logout,RefreshAccessToken,Register,Profile, UpdateProfile, ProfileChangePassword} from "../controllers/user.controller.js";
+import { Login ,Logout,RefreshAccessToken,Register,Profile, UpdateProfile, ProfileChangePassword,systemBLogin,systemALogin} from "../controllers/user.controller.js";
 
 import verifyToken from "../middleware/auth.middleware.js";
 import authorizeRoles from "../middleware/checkRole.js";
@@ -12,7 +12,11 @@ const router = Router()
 
 
 router.route("/register").post(Register)
+
 router.route("/login").post(Login)
+router.post('/systemA/login', systemALogin);
+router.post('/systemB/login', systemBLogin);
+router.post('/logout', logout);
 router.route("/profile").get(verifyToken,authorizeRoles(["admin","user"]),Profile)
 router.route("/profile-update").put(verifyToken,authorizeRoles(["admin","user"]),UpdateProfile)
 router.route("/profile-change-password").put(verifyToken,authorizeRoles(["admin","user"]),ProfileChangePassword)
