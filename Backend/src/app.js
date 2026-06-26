ee3841a205b4a80adde65bdf5968e6dc64dda951
mukulkumawat2412 <154887138+mukulkumawat2412@users.noreply.github.com>
2026-06-26 17:09:25 +0530

    Change morgan logging format from 'combined' to 'dev'

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(helmet())
 app.use(compression())
-app.use(morgan("combined"))
+app.use(morgan("dev"))
 
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("public/temp"));
